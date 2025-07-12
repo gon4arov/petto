5190562de642f32bf1bf9cec7b3a61759a0db325
Sago Mac <pomprok22@gmail.com>
2025-07-12 11:08:08 +0300

    films

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert(4);
+let numberOfFilms = +prompt("Сколько фильмов ты посмотрел?")
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: true
+
+};
+let ans1 = prompt('Один из последних просмотренных фильмов?');
+let ans2 = +prompt('На сколько оцените его?');
+let ans3 = prompt('Один из последних просмотренных фильмов?');
+let ans4 = +prompt('На сколько оцените его?');
+
+personalMovieDB.movies[ans1] = ans2;
+personalMovieDB.movies[ans3] = ans4;
+
+console.log(personalMovieDB);
