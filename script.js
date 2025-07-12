bf43fbe95767c349c7b2f33ea24416b86eda3433
Sago Mac <pomprok22@gmail.com>
2025-07-12 11:18:39 +0300

    films with hints

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ let personalMovieDB = {
     privat: false
 
 };
-let ans1 = prompt('Один из последних просмотренных фильмов?');
-let ans2 = +prompt('На сколько оцените его?');
-let ans3 = prompt('Один из последних просмотренных фильмов?');
-let ans4 = +prompt('На сколько оцените его?');
+let ans1 = prompt('Один из последних просмотренных фильмов?', '');
+let ans2 = +prompt('На сколько оцените его?', '');
+let ans3 = prompt('Один из последних просмотренных фильмов?', '');
+let ans4 = +prompt('На сколько оцените его?', '');
 
 personalMovieDB.movies[ans1] = ans2;
 personalMovieDB.movies[ans3] = ans4;
